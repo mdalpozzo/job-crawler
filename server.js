0546f99523c2f873282dee482ec5ccbc92b569fc
Marlin Dalpozzo <marlin.dalpozzo@gmail.com>
2019-02-19 17:19:58 -0800

    adds args to puppeteer.launch to work with heroku

--- server.js
+++ server.js
@@ -29,7 +29,10 @@ app.get('/url', (req, res) => {
 
   (async () => {
     try {
-      const browser = await puppeteer.launch({ headless: false });
+      const browser = await puppeteer.launch({
+        headless: false,
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+      });
       const page = await browser.newPage();
       await page.goto(url, { waitUntil: 'networkidle2' });
       page.setUserAgent(
